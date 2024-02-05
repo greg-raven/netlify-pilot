df3126e219d35e21fdc063000970dd89117c4103
Greg Raven <gregraven@mac.com>
2024-02-05 05:11:59 -0800

    Update jtoc.js

--- _assets/jTOC/jtoc.js
+++ _assets/jTOC/jtoc.js
@@ -1,179 +1,115 @@
-/*
- * jTOC - jQuery/Jeremy's Table of contents.
- *
- * https://github.com/jez500/jtoc
- * v0.1
- *
- * Builds a table of contents based on headings on the page.
- *
- * Usage:
- * Call on a ul/ol and it will get populated with TOC links
- *
- * Example:
- * $('ul.toc').jtoc({content: '.content', headings: 'h2,h3'});
- *
- * Options:
- * - content: The selector to search for headings in. Default: 'body'
- * - headings: Headings to search for. Default: "h1,h2,h3"
- * - anchorPrefix: Optionally add a prefix to anchors.
- * - scrollEnabled: Should there be smooth scrolling to headings. Default true
- * - scrollSpeed: Speed of animation when scrolling to headings. Default 400
- * - scrollEase: Scroll animation. Default: 'swing'
- * - scrollEl: The element that scrolls. Default: 'body,html'
- * - scrollOffset: A pixel offset for scroll. Default 0
- *
- * Inspiration/Credits to:
- * - https://github.com/firstandthird/smooth-scroller
- * - https://github.com/ndabas/toc
- * - https://github.com/jeremy-doghouse/toc
- */
-(function($) {
+(function ($) {
+    "use strict";
 
-  "use strict";
-
-  /**
-   * jTOC class.
-   */
-  var jTOC = function(dom, options) {
-    var self = this,
-      defaultOpts;
-
-    // Setup options.
-    defaultOpts = {
-      content: "body",
-      headings: "h1,h2,h3",
-      anchorPrefix: '',
-      scrollEnabled: true,
-      scrollSpeed: 400,
-      scrollEase: 'swing',
-      scrollEl: 'body,html',
-      scrollOffset: 0
-    };
-    self.opts = $.extend(defaultOpts, options);
-
-    /*
-     * Initialise the class.
-     */
-    self.init = function () {
-      // Init some vars we'll need.
-      self.$dom = $(dom);
-      self.stack = [self.$dom];
-      self.listTag = dom.tagName;
-      self.currentLevel = 0;
-      self.headingSelectors = self.opts.headings.split(',');
-
-      // Parse and return self for chaining.
-      self.parseContent();
-
-      // Add a trigger for other scripts to do their thing.
-      self.$dom.trigger('jTocBuilt');
-
-      return self;
-    };
-
-    /*
-     * Parse the content into a nested list.
-     */
-    self.parseContent = function () {
-      // Set up some automatic IDs if we do not already have them
-      $(self.opts.content).find(self.opts.headings).each(function () {
-        var $el = $(this), anchorName, $a, level, parentItem;
-
-        // What level is the current heading?
-        level = $.map(self.headingSelectors, function (selector, index) {
-          return $el.is(selector) ? index : undefined;
-        })[0];
-
-        // Generate a valid ID: must start with a letter, and contain only letters and
-        // numbers. All other characters are replaced with underscores.
-        anchorName = self.opts.anchorPrefix + $el.text().replace(/^[^A-Za-z]*/, "").replace(/[^A-Za-z0-9]+/g, "_");
-
-        // Add anchor.
-        if ($el.attr('id') !== anchorName) {
-          $('<span/>').attr('id', anchorName).insertBefore($el);
+    class jTOC {
+        constructor(dom, options) {
+            this.$dom = $(dom);
+            this.opts = $.extend(this.getDefaultOptions(), options);
+            this.currentLevel = 0;
+            this.headingSelectors = this.opts.headings.split(",");
+            this.stack = [this.$dom];
+            this.listTag = dom.tagName;
+            this.init();
         }
 
-        if (level > self.currentLevel) {
-          // If the heading is at a deeper level than where we are, start a new nested
-          // list, but only if we already have some list items in the parent. If we do
-          // not, that means that we're skipping levels, so we can just add new list items
-          // at the current level.
-          // In the upside-down stack, unshift = push, and stack[0] = the top.
-          parentItem = self.stack[0].children("li:last")[0];
-          if (parentItem) {
-            self.stack.unshift($("<" + self.listTag + "/>").appendTo(parentItem));
-          }
-        } else {
-          // Truncate the stack to the current level by chopping off the 'top' of the
-          // stack. We also need to preserve at least one element in the stack - that is
-          // the containing element.
-          self.stack.splice(0, Math.min(self.currentLevel - level, Math.max(self.stack.length - 1, 0)));
+        getDefaultOptions() {
+            return {
+                content: "body",
+                headings: "h1,h2,h3",
+                anchorPrefix: "",
+                scrollEnabled: true,
+                scrollSpeed: 400,
+                scrollEase: "swing",
+                scrollEl: "body,html",
+                scrollOffset: 0
+            };
         }
 
-        // Build TOC link.
-        $a = $('<a/>')
-          .text($el.text())
-          .attr('href', '#' + anchorName)
-          .addClass('toc-link toc-link--level-' + level)
-          .bind('click', function (e) {
-            // If scroll enabled.
-            if (self.opts.scrollEnabled === true) {
-              e.preventDefault();
-              self.scrollTo('#' + anchorName);
+        init() {
+            this.parseContent();
+            this.$dom.trigger("jTocBuilt");
+        }
+
+        parseContent() {
+            const self = this;
+            $(self.opts.content).find(self.opts.headings).each(function () {
+                const $el = $(this);
+                const level = self.getHeadingLevel($el);
+
+                const anchorName = self.generateAnchorName($el);
+                self.addAnchorIfNeeded($el, anchorName);
+
+                self.updateStack(level);
+
+                const $a = self.buildTOCLink($el, anchorName, level);
+                $("<li/>").appendTo(self.stack[0]).append($a);
+
+                self.currentLevel = level;
+            });
+        }
+
+        getHeadingLevel($el) {
+            return $.map(this.headingSelectors, (selector, index) => $el.is(selector) ? index : undefined)[0];
+        }
+
+        generateAnchorName($el) {
+            return this.opts.anchorPrefix + $el.text().replace(/^[^A-Za-z]*/, "").replace(/[^A-Za-z0-9]+/g, "_");
+        }
+
+        addAnchorIfNeeded($el, anchorName) {
+            if ($el.attr("id") !== anchorName) {
+                $("<span/>").attr("id", anchorName).insertBefore($el);
             }
-            $el.trigger('selected', $(this).attr('href'));
-            $(this).blur();
-          });
-
-        // Add the list item
-        $("<li/>").appendTo(self.stack[0]).append($a);
-
-        self.currentLevel = level;
-      });
-
-      return self;
-    };
-
-    /*
-     * ScrollTo element method.
-     * @see https://github.com/firstandthird/smooth-scroller
-     */
-    self.scrollTo = function (selector) {
-      var $el = $(selector);
-
-      $(self.opts.scrollEl).animate({
-        scrollTop: $el.offset().top - $(self.opts.scrollEl).offset().top - self.opts.scrollOffset
-      }, self.opts.scrollSpeed, self.opts.scrollEase, function () {
-        var hash = $el.attr('id');
-
-        if (hash.length) {
-          if (history.pushState) {
-            history.pushState(null, null, '#' + hash);
-          } else {
-            document.location.hash = hash;
-          }
         }
 
-        $el.trigger('scrollComplete');
-      });
+        updateStack(level) {
+            if (level > this.currentLevel) {
+                const parentItem = this.stack[0].children("li:last")[0];
+                if (parentItem) {
+                    this.stack.unshift($("<" + this.listTag + "/>").appendTo(parentItem));
+                }
+            } else {
+                this.stack.splice(0, Math.min(this.currentLevel - level, Math.max(this.stack.length - 1, 0)));
+            }
+        }
 
-      return self;
+        buildTOCLink($el, anchorName, level) {
+            return $("<a/>")
+                .text($el.text())
+                .attr("href", "#" + anchorName)
+                .addClass(`toc-link toc-link--level-${level}`)
+                .on("click", (e) => {
+                    if (this.opts.scrollEnabled) {
+                        e.preventDefault();
+                        this.scrollTo("#" + anchorName);
+                    }
+                    $el.trigger("selected", $(this).attr("href"));
+                    $(this).blur();
+                });
+        }
+
+        scrollTo(selector) {
+            const $el = $(selector);
+            $(this.opts.scrollEl).animate({
+                scrollTop: $el.offset().top - $(this.opts.scrollEl).offset().top - this.opts.scrollOffset
+            }, this.opts.scrollSpeed, this.opts.scrollEase, () => {
+                const hash = $el.attr("id");
+                if (hash.length) {
+                    if (history.pushState) {
+                        history.pushState(null, null, "#" + hash);
+                    } else {
+                        document.location.hash = hash;
+                    }
+                }
+                $el.trigger("scrollComplete");
+            });
+        }
+    }
+
+    $.fn.jtoc = function (options) {
+        return this.each(function () {
+            new jTOC(this, options);
+        });
     };
 
-    // Initialize.
-    self.init();
-
-    // Return self for chaining.
-    return self;
-  };
-
-
-  // jQuery plugin.
-  $.fn.jtoc = function(options) {
-    this.each(function(i, d) {
-      new jTOC(d, options);
-    });
-    return this;
-  };
-
-}(jQuery));
+})(jQuery);
